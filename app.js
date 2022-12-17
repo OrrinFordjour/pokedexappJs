fa51a1442d2967c9f97b786a3ed3a6bf5f67cb5a
orrinfordjour <orrinfordjour1990@gmail.com>
2022-12-18 00:18:17 +0100

    pokemon init first commit (created index css js)

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+const pokedex = document.getElementById('pokedex')
 
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i < 152; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((response) => response.json()));
+  }
 
-const fetchPokemon = ()=> {
-    console.log('Fetching Pokemon.!');
-}
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((data) => ({
+      id: data.id,
+      name: data.name,
+      image: data.sprites["front_default"],
+      type: data.types.map((type) => type.type.name).join(", "),
+    }));
+    displayPokemon(pokemon);
+  });
+};
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon.map (pokeman => `
+  <div class="bg-gray-100 py-8 px-8 items-center text-center">
+    <img class="mx-auto hover:animate-bounce" src="${pokeman.image}"/>
+    <p class="uppercase text-[16px] font-normal">0${pokeman.id}. ${pokeman.name}</p>
+    <p class="font-extralight text-[#656] text-[13px]" >Type: ${pokeman.type}</p>
+  </div>
+  `).join('');
+  pokedex.innerHTML = pokemonHTMLString;
+
+};
 
 fetchPokemon();
